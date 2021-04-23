775cc31c0c92975f6a85568567e5e5928df6a3ed
harry212001 <harrison.mouat19@imperial.ac.uk>
2021-04-23 12:25:31 +0100

    Attempt 2

---
+++ exercise_02/Harry-icl/math_utils/primes.py
@@ -0,0 +1,11 @@
+import math
+
+def isprime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
